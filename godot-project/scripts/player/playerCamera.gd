cdb6a935a4db8738d2df5a074fe6728dc5d24a60
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2020-12-23 19:26:20 +0100

    some what of aiming? lul

--- godot-project/scripts/player/playerCamera.gd
+++ godot-project/scripts/player/playerCamera.gd
@@ -1,12 +1,26 @@
 extends Camera
 
+var aimFov = 50;
+var aimSpeed = 5;
+var aim = false;
+
 func _ready():
 	fov = Global.playerFov
 	far = Global.playerViewDistance
 
 func _process(delta):
-	if fov != Global.playerFov:
-			fov = Global.playerFov
+	if aim:
+		if fov > aimFov:
+			fov -= aimSpeed;
+	else:
+		if fov < Global.playerFov:
+			fov += aimSpeed;
+	
+#	if fov != Global.playerFov:
+#		fov = Global.playerFov
 			
 	if far != Global.playerViewDistance:
 		far = Global.playerViewDistance
+
+func _on_KinematicBody_aim(state):
+	aim = state;
